94351ab5ed5c9ee76cbb417d2879518ec31ddef2
Marion Mota <mmo@DQESOFT.local>
2020-02-21 10:33:24 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+import os
+
+if __name__ == "__main__":
+    #set the screen
+    wn = turtle.Screen()
+    wn.bgcolor("black")
+    wn.title("Space Invaders")
+
+    #draw border
+    border_pen = turtle.Turtle()
+    border_pen = pen.speed(0)
+    border_pen = ppen.color("White")
+    #delay = raw_input("Press enter ton finish.")
